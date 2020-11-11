7da00bbafcaf78c39ba5e24b57ea77a35fa06372
moskala <moskal.ala@gmail.com>
2020-11-11 03:25:47 +0100

    Add show functions

---
+++ Methods/Show_matplotlib_all.py
@@ -0,0 +1,42 @@
+import nibabel
+import matplotlib.pyplot as plt
+import pydicom
+import matplotlib.image as mpimg
+
+
+def show_dicom(file_path):
+    dcm = pydicom.dcmread(file_path)
+
+    plt.axis('off')
+    plt.imshow(dcm.pixel_array, cmap='gray')
+    plt.show()
+
+
+def show_nii(file_path, value):
+    ct = nibabel.load(file_path)
+    ct_array = ct.get_fdata()
+    ct_array = ct_array.T
+
+    if value < 0:
+        value = 0
+    elif value >= len(ct_array):
+        value = len(ct_array) - 1
+
+    plt.axis('off')
+    plt.imshow(ct_array[value], cmap='gray')
+    plt.show()
+
+
+def show_jpg_png(file_path):
+    img = mpimg.imread(file_path)
+
+    plt.axis('off')
+    plt.imshow(img)
+    plt.show()
+
+
+# Przykłady:
+# show_dicom(r"D:\Studia\sem7\inzynierka\data\covidctscans_org\16_Italy\Original Dicom\Italy_case010000.dcm")
+# show_nii(r"D:\Studia\sem7\inzynierka\data\segmentation\ct_scan\coronacases_org_001.nii", 5)
+# show_jpg_png(r'D:\Studia\sem7\inzynierka\data\test\test\0.jpg')
+# show_jpg_png(r'D:\Studia\sem7\inzynierka\data\test\test\2237.png')
